236684d07d8362a15ffebca87595d6ec59e054f3
hiroyuki nagata <hiroyuki@cowboy.freestylewiki.xyz>
2019-03-30 10:14:57 +0900

    Update sample query with MySQL employee table

--- py-sql-combos/constants.py
+++ py-sql-combos/constants.py
@@ -1,1 +1,8 @@
-SAMPLE_QUERY = """select * from sample;"""
+SAMPLE_QUERY = """SELECT
+e.name AS employee_name
+, e.email AS employee_email
+, e.name AS department_name
+FROM employee e
+
+LEFT JOIN department d
+ON e.id = d.employee_id;"""
