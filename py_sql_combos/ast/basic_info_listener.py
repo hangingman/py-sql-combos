c1881f3efbd08c048dec74f1c58889f3e987038a
hiroyuki nagata <newserver002@gmail.com>
2019-04-29 21:16:39 +0900

    JOINに必要な条件を抽出

--- py_sql_combos/ast/basic_info_listener.py
+++ py_sql_combos/ast/basic_info_listener.py
@@ -8,36 +8,50 @@ class BasicInfoListener(MySqlParser):
         self.ast_info = {
             'root': "",
             'sql_statements': [],
+            'join_parts': [],
+            'from_clause': "",
+            'select_elements': []
         }
 
 
+    # クエリ全体
     def enterRoot(self, ctx:MySqlParser.RootContext):
-        #print("Enter root")
-        #print("  " + ctx.getText())
         self.ast_info['root'] = ctx.getText()
 
-
     def exitRoot(self, ctx:MySqlParser.RootContext):
-        #print("Exit root")
         pass
 
-
+    # ; までのクエリ
     def enterSqlStatements(self, ctx:MySqlParser.SqlStatementsContext):
-        #print("  Enter sql_statements")
-        #print("    " + ctx.getText())
         self.ast_info['sql_statements'].append(ctx.getText())
 
-
     def exitSqlStatements(self, ctx:MySqlParser.SqlStatementsContext):
-        #print("  Exit sql_statements")
+        pass
+
+    # select句のすべての要素
+    def enterSelectElements(self, ctx:MySqlParser.SelectElementsContext):
+        self.ast_info['select_elements'].append(ctx.getText())
+
+    def exitSelectElements(self, ctx:MySqlParser.SqlStatementsContext):
+        pass
+
+    def enterFromClause(self, ctx:MySqlParser.FromClauseContext):
+        self.ast_info['from_clause'] = ctx.getText()
+
+    def enterJoinPart(self, ctx:MySqlParser.JoinPartContext):
+        self.ast_info['join_parts'].append(ctx.getText())
+
+    def exitJoinPart(self, ctx:MySqlParser.JoinPartContext):
         pass
 
 
     def enterEveryRule(self, ctx):
         pass
 
+
     def exitEveryRule(self, ctx):
         pass
+
 
     def visitTerminal(self, ctx):
         pass
