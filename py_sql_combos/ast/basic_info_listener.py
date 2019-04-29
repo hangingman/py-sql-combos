725cca5208f567df1817244cd7e4a3526114cfd8
hiroyuki nagata <newserver002@gmail.com>
2019-04-29 20:55:12 +0900

    Listenerパターンを使用する

--- py_sql_combos/ast/basic_info_listener.py
+++ py_sql_combos/ast/basic_info_listener.py
@@ -6,12 +6,13 @@ class BasicInfoListener(MySqlParser):
 
     def __init__(self):
         self.call_methods = []
+        self.root = ""
 
     def enterRoot(self, ctx:MySqlParser.RootContext):
-        print("**1")
-        print(ctx.getText())
-        print(ctx.toStringTree())
-        print("***")
+        print("enter root")
+        print("  " + ctx.getText())
+        self.root = ctx.getText()
+        print("exit root")
 
     def enterEveryRule(self, ctx):
         print("enter every rule")
