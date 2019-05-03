b9b970249da6871be83005fc8e028440ba0be296
hiroyuki.nagata <hiroyuki.nagata@hiroyuki-nagatas-MacBook-Pro.local>
2019-05-03 16:07:26 +0900

    テーブル名を明示的に取得できるようになった

--- py_sql_combos/ast/basic_info_listener.py
+++ py_sql_combos/ast/basic_info_listener.py
@@ -9,6 +9,7 @@ class BasicInfoListener(MySqlParser):
             'root': "",
             'sql_statements': [],
             'join_parts': [],
+            'table_sources': [],
             'from_clause': "",
             'select_elements': []
         }
@@ -18,22 +19,13 @@ class BasicInfoListener(MySqlParser):
     def enterRoot(self, ctx:MySqlParser.RootContext):
         self.ast_info['root'] = ctx.getText()
 
-    def exitRoot(self, ctx:MySqlParser.RootContext):
-        pass
-
     # ; までのクエリ
     def enterSqlStatements(self, ctx:MySqlParser.SqlStatementsContext):
         self.ast_info['sql_statements'].append(ctx.getText())
 
-    def exitSqlStatements(self, ctx:MySqlParser.SqlStatementsContext):
-        pass
-
     # select句のすべての要素
     def enterSelectElements(self, ctx:MySqlParser.SelectElementsContext):
         self.ast_info['select_elements'].append(ctx.getText())
-
-    def exitSelectElements(self, ctx:MySqlParser.SqlStatementsContext):
-        pass
 
     def enterFromClause(self, ctx:MySqlParser.FromClauseContext):
         self.ast_info['from_clause'] = ctx.getText()
@@ -42,14 +34,17 @@ class BasicInfoListener(MySqlParser):
     def enterOuterJoin(self, ctx:MySqlParser.OuterJoinContext):
         self.ast_info['join_parts'].append(ctx.getText())
 
-    def exitOuterJoin(self, ctx:MySqlParser.JoinPartContext):
-        pass
-
     # Joinは以下の４つある
     # InnerJoinContext
     # StraightJoinContext
     # OuterJoinContext
     # NaturalJoinContext
+
+    # JOINに使用しているテーブルソース
+    def enterTableName(self, ctx:MySqlParser.TableNameContext):
+        table_name = ctx.getText()
+        if table_name not in self.ast_info['table_sources']:
+            self.ast_info['table_sources'].append(table_name)
 
     # 以下、動作に必要なメソッド
     def enterEveryRule(self, ctx):
